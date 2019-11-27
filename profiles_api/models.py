89b18018c46ec73ef105300379301f58e2ad2cfd
vinay <v.vinay317@gmail.com>
2019-11-28 00:03:15 +0530

    Changes to model and super user

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -20,11 +20,11 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_super_user(self, email, name, password):
+    def create_superuser(self, email, name, password):
         """Create and save a new super user with given details"""
         user = self.create_user(email, name, password)
 
-        user.is_super_user = True
+        user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
 
