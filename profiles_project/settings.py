5a586938b0b5ccbbc52d45f08ffa46f0baf1d3b2
vinay <v.vinay317@gmail.com>
2019-11-27 23:49:20 +0530

    Added Profile manager and models

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Is this correct ? Not profiles_api.models.UserProfile ?
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
