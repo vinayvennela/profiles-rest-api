f084b40d579752e2992bf17f4789c0a2d39ac811
vinay <v.vinay317@gmail.com>
2019-11-29 00:28:31 +0530

    Added Post,put,path and delete methods to our api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -13,5 +16,31 @@ class HelloApiView(APIView):
             'Gives you the most control over your application logic',
             'Is mapped manually to URLs',
         ]
-        """Either dictionary or lisy which will be converted to JSON"""
+        """Either dictionary or list which will be converted to JSON"""
         return Response({'message':'Hello!','an_apiview':an_apiview})
+
+    def post(self, request):
+        """Create a Hello Message with our name """
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(
+            serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method':'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method':'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Handle delete of an object"""
+        return Response({'method':'DELETE'})
